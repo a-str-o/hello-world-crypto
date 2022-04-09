70189f62528c60789610a109ffea7eaa035eedba
Younes Elguerraoui <yoelguer@e2r9p12.1337.ma>
2022-04-09 22:11:57 +0000

    test

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import React , {useState} from 'react';
 import { ethers } from 'ethers';
 import HelloWorld_abi from './hello_abi.json';
+import './components/button.css';
 
 function App() {
 
-  const contractAdress = '0x76C3353c48e333c329B2A2c7632669352ADcae9A';
+  const contractAdress = '0x8BdC4b1Dc049880A29f624361Cd6B39A255E55e6';
 
   const [errorMessage, setErrorMessage] = useState(null);
-  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState("0x0000000000000000000000000000000000000000");
   const [connectButtonText, setConnectButtonText] = useState('Connect Wallet');
-  const [currentContractVal, setCurrentContractVal] = useState(null);
+  const [currentContractVal, setCurrentContractVal] = useState("you'r message will display here");
 
   const [provider, setProvider] = useState(null);
   const [singner, setSingner] = useState(null);
@@ -48,31 +49,51 @@ function App() {
   const setHandler = (e) => {
     e.preventDefault();
     contract.set(e.target.setText.value);
+    console.log(e.target.setText.value);
   }
   const getCurrentVal = async () => {
     let val = await contract.get();
     setCurrentContractVal(val);
   }
+  var animateButton = function(e) {
+    e.preventDefault();
+     //reset animation
+     e.target.classList.remove('animate');
+     e.target.classList.add('animate');
+     setTimeout(function(){
+       e.target.classList.remove('animate');
+     },700);
+   };
+   
+   var bubblyButtons = document.getElementsByClassName("bubbly-button");
+   var i = 0
+   while (i < bubblyButtons.length)
+   {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+    i++;
+   }
   return (
     <div className="container-body">
       <div className="container-card">
         <div className="card"> 
             <h3 className="text-title"> Get/set Interaction with contract ! "</h3>
-              <button className="awesome-button" onClick={connectWalletHandler}>
+            <div className="flex-div">
+              <button className="bubbly-button" onClick={connectWalletHandler}>       
                 {connectButtonText}
               </button>
               <h3 className="text-title">Adress: {defaultAccount}</h3>
+            </div>
               <form onSubmit={setHandler}>
                 <div className="flex-div">
-                  <input className="input-text" id="setText" type="text" />
-                  <button  className="awesome-button" type={"submit"}>Update Contract</button>
+                  <button  className="bubbly-button" type={"submit"}>Update Contract</button>
+                  <input  className="input-text"  placeholder="set your message to contract here"/>
                 </div>
               </form>
-              <button onClick={getCurrentVal}> Get Current Value </button>
-              <h3 className="text-title">
-              {currentContractVal}
+              <div className="flex-div">
+                <button className="bubbly-button" onClick={getCurrentVal}> Get Current Value </button>
+                <h3 className="text-title">{currentContractVal}</h3>
+              </div>
               {errorMessage}
-              </h3>
             </div>
       </div> 
     </div>
