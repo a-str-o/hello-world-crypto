7d300e1f574b2f26af6e3367fcb2f607c64a3c75
Younes Elguerraoui <yoelguer@e2r9p12.1337.ma>
2022-04-10 06:36:42 +0000

    finish

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './components/button.css';
 
 function App() {
 
-  const contractAdress = '0x8BdC4b1Dc049880A29f624361Cd6B39A255E55e6';
+  const contractAdress = '0xD86cea4e650347057397af28EB5Fbe961ABe20C2';
 
   const [errorMessage, setErrorMessage] = useState(null);
   const [defaultAccount, setDefaultAccount] = useState("0x0000000000000000000000000000000000000000");
@@ -49,49 +49,46 @@ function App() {
   const setHandler = (e) => {
     e.preventDefault();
     contract.set(e.target.setText.value);
-    console.log(e.target.setText.value);
+    setTimeout(() => {  alert("contract was updated");; }, 5000);
+    
   }
+
   const getCurrentVal = async () => {
     let val = await contract.get();
     setCurrentContractVal(val);
-  }
+  } 
+ 
   var animateButton = function(e) {
-    e.preventDefault();
-     //reset animation
+     e.preventDefault();
      e.target.classList.remove('animate');
      e.target.classList.add('animate');
-     setTimeout(function(){
-       e.target.classList.remove('animate');
-     },700);
+     setTimeout(function(){e.target.classList.remove('animate');},1000);
    };
-   
-   var bubblyButtons = document.getElementsByClassName("bubbly-button");
-   var i = 0
-   while (i < bubblyButtons.length)
-   {
-    bubblyButtons[i].addEventListener('click', animateButton, false);
-    i++;
-   }
+    var bubblyButtons = document.getElementsByClassName("bubbly-button");
+    for (var i = 0; i < bubblyButtons.length; i++)
+    {
+      bubblyButtons[i].addEventListener('click', animateButton, false);
+    }
   return (
     <div className="container-body">
       <div className="container-card">
         <div className="card"> 
-            <h3 className="text-title"> Get/set Interaction with contract ! "</h3>
+            <h3 className="text-title">Get/set Interaction with contract ! "</h3>
             <div className="flex-div">
+              <h3 className="text-title">Adress: {defaultAccount}</h3>
               <button className="bubbly-button" onClick={connectWalletHandler}>       
                 {connectButtonText}
               </button>
-              <h3 className="text-title">Adress: {defaultAccount}</h3>
             </div>
-              <form onSubmit={setHandler}>
-                <div className="flex-div">
-                  <button  className="bubbly-button" type={"submit"}>Update Contract</button>
-                  <input  className="input-text"  placeholder="set your message to contract here"/>
-                </div>
-              </form>
+            <form onSubmit={setHandler}>
+              <input  id="setText" type="text" />
+              <button className="butt" type={"submit"}>Update Contract</button>
+            </form>
               <div className="flex-div">
-                <button className="bubbly-button" onClick={getCurrentVal}> Get Current Value </button>
                 <h3 className="text-title">{currentContractVal}</h3>
+                <button className="bubbly-button" onClick={getCurrentVal}> 
+                    Get Current Value 
+                </button>
               </div>
               {errorMessage}
             </div>
